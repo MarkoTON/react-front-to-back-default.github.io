5c561042d5e3bce39f66895ea3d213808c1df073
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-01 15:42:24 +0200

    adding bootstrap classes

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +10,11 @@ class App extends Component {
     return (
       <div className="App">
         {/* <Header  /> */}
-        <Header branding="Contact Manager!" />
-        <Contact name="John Doe" email="jonD@gmail.com" phone="555-44-33" />
-        <Contact name="Marko C" email="marko@gmail.com" phone="698-98-65" />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="John Doe" email="jonD@gmail.com" phone="555-44-33" />
+          <Contact name="Marko Canovic" email="marko@gmail.com" phone="698-98-65" />
+        </div>
       </div>
     );
   }
