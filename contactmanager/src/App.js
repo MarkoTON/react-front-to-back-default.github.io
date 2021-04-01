00f08b6c285912c6d011285637e889775bb935eb
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-01 14:44:34 +0200

    adding first component

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <p>The App Component</p>
         <p>Creating new component</p>
+        <Contact />
       </div>
     );
   }
---
+++ contactmanager/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Jonh Doe</h4>
+        <ul>
+          <li>Email: johnD@gmail.com</li>
+          <li>Phone: 555-555-55</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
