f2e4c84b1405d44e57b186804873086b0e4e9d76
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-01 14:24:25 +0200

    small test and example

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -3,9 +3,29 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let name = 'Larry';
+    let showHello = true;
+    let showMath = true;
+    let num1 = 40;
+    let num2 = 33;
+
+    let math;
+    if(showMath){
+      math = <h4>{num1} + {num2} = {num1 + num2}</h4>
+      // Pravilno se pise kao sto je dole napisano
+      // math = (
+      // <h4>{num1} + {num2} = {num1 + num2}</h4>
+      // );
+
+    } else {
+      math = null;
+    }
+
     return (
       <div className="App">
         <p>The App Component</p>
+        {showHello ? <h4>Hello {name.toUpperCase()}</h4> : <p>:P</p>}
+        {math}
       </div>
     );
   }
