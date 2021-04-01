a5681c11f9d085a039b501edd92bfd907ef8c709
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-01 18:36:42 +0200

    change state over props

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
   static propTypes = {
-    contact: PropTypes.object.isRequired
+    contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
   };
 
   state = {
@@ -19,6 +20,11 @@ class Contact extends Component {
     // console.log(this.state.showContactInfo);
   }
 
+  onDeleteClick = (e) => {
+    console.log('click');
+    this.props.deleteClickHandler();
+  }
+
   
   render() {
     const {email, name, phone} =  this.props.contact;
@@ -27,7 +33,10 @@ class Contact extends Component {
     return (
       <div className="card card-body mb-3">
         {/* <h4>Name: {name} <i onClick={this.onShowClick.bind(this,name)} className="fas fa-sort-down"></i></h4> */}
-        <h4>Name: {name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i></h4>
+        <h4>Name: {name} 
+          <i style={{cursor: 'pointer'}} onClick={this.onShowClick} className="fas fa-sort-down"></i>
+          <i className="fas fa-times" onClick={this.onDeleteClick} style={{cursor: 'pointer', float: 'right', color: 'red'}}></i>
+        </h4>
         {showContactInfo ? 
           (<ul className="list-group">
             <li className="list-group-item">Email: {email}</li>
--- contactmanager/src/components/Contacts.js
+++ contactmanager/src/components/Contacts.js
@@ -12,6 +12,18 @@ class Contacts extends Component {
         { id:3, name: "Milos", email: "milos@gmail.com", phone: '449-999-21' }
       ]
     }
+  };
+
+  deleteContact = (id) => {
+    const { contacts } = this.state;
+
+    const newContacts = contacts.filter(contact => {
+      return contact.id !== id;
+    });
+
+    this.setState({ contacts: newContacts });
+    // console.log(id);
+    // console.log("object");
   }
 
   render() {
@@ -20,7 +32,7 @@ class Contacts extends Component {
     return (
       <React.Fragment>
         {contacts.map(contact => (
-          <Contact contact={contact} key={contact.id} />
+          <Contact contact={contact} key={contact.id} deleteClickHandler={this.deleteContact.bind(this, contact.id)} />
         ))} 
       </React.Fragment>
     )
